0047adcb28b2ef465df0d816cd2a9c3f2ac26e44
Nery Daniel Vásquez y Vásquez-0905-23-15654 <nvasquezy1@miumg.edu.gt>
2025-03-22 09:17:03 -0600

    Add files via upload

---
+++ Darkblade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using p1bpoo.MisClases;
+public class Darkblade : Vehiculo
+{
+    public Darkblade(int anio, string elcolor, string elmodelo) : base(anio, elcolor, elmodelo)
+    {
+        TiposdelicenciaAceptados = new List<string> { "M" };
+    }
+}
+
---
+++ runing.cs
@@ -0,0 +1,8 @@
+﻿using p1bpoo.MisClases;
+public class runing : Vehiculo
+{
+    public runing(int anio, string elcolor, string elmodelo) : base(anio, elcolor, elmodelo)
+    {
+        TiposdelicenciaAceptados = new List<string> { "A", "b", "c" };
+    }
+}
